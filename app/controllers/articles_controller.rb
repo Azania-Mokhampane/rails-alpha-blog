797a8427b65874ee4b36a762c2de43e6b781783f
Azania-Mokhampane <azaniam04@gmail.com>
2024-12-02 20:40:51 +0200

    feat: show all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,12 @@
 class ArticlesController < ApplicationController
+
   def show
     # debugger
-    # instance variable
+    # instance variable so that we can access it in the view or throughout the application
     @article =  Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
   end
 end
